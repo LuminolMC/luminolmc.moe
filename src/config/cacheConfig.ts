9a2a2cee57b6b6357ad9dc06b6af2ef79b2de83a
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-10-06 16:53:37 +0800

    feat: add cn cache port

--- src/config/cacheConfig.ts
+++ src/config/cacheConfig.ts
@@ -11,6 +11,11 @@ const cacheConfigs: CacheConfig[] = [
         oldUrl: 'https://luminolmc.buildmanager.api.blue-millennium.fun/github_releases.old.json'
     },
     {
+        name: 'Cache_China',
+        url: 'https://36.50.226.140/github_releases.json',
+        oldUrl: 'https://36.50.226.140/github_releases.old.json'
+    },
+    {
         name: 'Cache_Swiss',
         url: 'https://api.xiamohuahuo.love/github_releases.json',
         oldUrl: 'https://api.xiamohuahuo.love/github_releases.old.json'
